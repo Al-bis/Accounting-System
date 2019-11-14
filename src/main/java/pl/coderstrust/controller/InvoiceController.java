52681d8f2bcb828535fe1a3406505b46da650a2c
Al-bis <albert.brandt123@gmail.com>
2019-11-14 21:30:13 +0100

    Added logging to console and to log file.

--- src/main/java/pl/coderstrust/controller/InvoiceController.java
+++ src/main/java/pl/coderstrust/controller/InvoiceController.java
@@ -3,6 +3,8 @@ package pl.coderstrust.controller;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.format.annotation.DateTimeFormat.ISO;
 import org.springframework.http.HttpStatus;
@@ -25,19 +27,25 @@ import java.util.Collection;
 @Api(tags = "Operations on Invoices")
 public class InvoiceController {
 
+    private static Logger LOGGER = LoggerFactory.getLogger(InvoiceController.class);
+
     private final InvoiceService invoiceService;
 
     public InvoiceController(InvoiceService invoiceService) {
         if (invoiceService == null) {
             throw new IllegalArgumentException("Given service cannot be null");
         }
+        LOGGER.info("Starting Invoice Controller.");
         this.invoiceService = invoiceService;
     }
 
     @ApiOperation(value = "Get all invoices", notes = "This function gets all available invoices")
     @GetMapping("/invoices")
     public ResponseEntity<Collection<Invoice>> getAllInvoices() {
-        return ResponseEntity.ok(ModelsMapper.mapInvoices(invoiceService.getAllInvoices()));
+        LOGGER.info("Request: GET/api/invoices. Get all invoices.");
+        Collection<Invoice> invoices = ModelsMapper.mapInvoices(invoiceService.getAllInvoices());
+        LOGGER.info("Response: All invoices returned in size of " + invoices.size() + ".");
+        return ResponseEntity.ok(invoices);
     }
 
     @ApiOperation(value = "Get invoices based on given date range",
@@ -48,15 +56,21 @@ public class InvoiceController {
         @ApiParam(value = "From date", example = "2019-01-23") LocalDate fromDate,
         @DateTimeFormat(iso = ISO.DATE) @RequestParam(value = "to")
         @ApiParam(value = "To date", example = "2019-01-24") LocalDate toDate) {
-        return ResponseEntity
-            .ok(ModelsMapper.mapInvoices(invoiceService.getAllInvoices(fromDate, toDate)));
+        LOGGER.info("Request: GET/api/invoices/date?from=" + fromDate + "to=" + toDate
+            + ". Get invoices in given range.");
+        Collection<Invoice> invoices = ModelsMapper.mapInvoices(invoiceService
+            .getAllInvoices(fromDate, toDate));
+        LOGGER.info("Response: Invoices returned in size of " + invoices.size() + ".");
+        return ResponseEntity.ok(invoices);
     }
 
     @ApiOperation(value = "Save Invoice", notes = "This function save given invoice")
     @PostMapping("/invoices")
     public ResponseEntity<Long> saveInvoice(@RequestBody @ApiParam
         (value = "This is invoice which will be saved in system") Invoice invoice) {
+        LOGGER.info("Request: POST/api/invoices. Save invoice: " + invoice);
         Long invoiceId = invoiceService.saveInvoice(ModelsMapper.mapInvoiceToDomain(invoice));
+        LOGGER.info("Response: Invoice was saved with ID = " + invoiceId + ".");
         return ResponseEntity.ok(invoiceId);
     }
 
@@ -64,10 +78,13 @@ public class InvoiceController {
     @GetMapping("/invoices/{id}")
     public ResponseEntity<Invoice> getInvoice(@PathVariable @ApiParam(value = "Invoice ID",
         example = "1") Long id) {
+        LOGGER.info("Request: GET/api/invoices/" + id + ". Get invoice under given ID.");
         Invoice invoice = ModelsMapper.mapInvoice(invoiceService.getInvoice(id));
         if (invoice != null) {
+            LOGGER.info("Response: Returned invoice: " + invoice + ".");
             return ResponseEntity.ok(invoice);
         } else {
+            LOGGER.error("Response: Invoice notFound. Given invoice was Null.");
             return ResponseEntity.notFound().build();
         }
     }
@@ -77,10 +94,14 @@ public class InvoiceController {
     @DeleteMapping("/invoices/{id}")
     public ResponseEntity<?> deleteInvoice(@PathVariable @ApiParam(value = "Invoice ID",
         example = "1") Long id) {
-        if (invoiceService.getInvoice(id) == null) {
+        LOGGER.info("Request: DELETE/api/invoices/" + id + ". Delete invoice under given ID");
+        Invoice invoice = ModelsMapper.mapInvoice(invoiceService.getInvoice(id));
+        if (invoice == null) {
+            LOGGER.error("Response: Invoice notFound. Given invoice was Null.");
             return ResponseEntity.notFound().build();
         }
         invoiceService.deleteInvoice(id);
+        LOGGER.info("Response: Deleted invoice: " + invoice + ".");
         return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 
